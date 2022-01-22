00229a049646c44b724f8db8e1d3a0c33722afc6
Akil Arsath <akil.j.arsath@gmail.com>
2022-01-22 11:27:35 +0530

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoiYWtpbGFyc2F0aCIsImEiOiJja3NxMnV2ajYwOTYzMnB0aHZ1NTN6bDM2In0.fdiHVL0nD-j4uGhqC5Ydmg&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude:body.features[0].geometry.coordinates[0],
+                longitude: body.features[0].geometry.coordinates[1],
+                location: body.query[0]
+            })
+        }
+    })
+}
+
+module.exports = geocode
+
